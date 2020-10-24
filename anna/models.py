adc6ac92ce6aaf0aad240ff765cb60b7455f1ae6
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-24 12:49:53 +0300

    imported users

--- anna/models.py
+++ anna/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='images')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image_url = models.URLField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
