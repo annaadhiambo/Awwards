493e45a469c1c2fc14351c05e6be2a195d8974f2
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-24 12:50:32 +0300

    imported post

--- anna/views.py
+++ anna/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+
+
+posts = [
+    {
+        'image':'',
+        'title':'Delani',
+        'description':'An application that allows you get to send Our approach unifies design, development and product management to create exceptional products.',
+        'image_url':'',
+        'author':'Anna Adhiambo',
+        'date_posted':'October 24,2020',
+    },
+    {
+        'image':'',
+        'title':'Gallery',
+        'description':'An application that allows you have new fashion in town',
+        'image_url':'',
+        'author':'Anna Adhiambo',
+        'date_posted':'October 24,2020'
+    },
+    {
+        'image':'',
+        'title':'Pizza',
+        'description':'An application that allows you have new fashion in town',
+        'image_url':'',
+        'author':'Anna Adhiambo',
+        'date_posted':'October 24,2020'
+    }
+]
 
 
 def home(request):
-    return render(request, 'photo/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'photo/home.html', context)
+
+
+
