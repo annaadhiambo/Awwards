59505211012d4e35f112fd133dde0e71fba905fc
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-25 09:35:47 +0300

    added posted detailviews

--- anna/urls.py
+++ anna/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
