842fec6278723d90373c4f5b172b131b1fdbfd00
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-25 17:57:12 +0300

    users/models.py

--- users/models.py
+++ users/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    @classmethod
+    def search_by_username(cls,search_term):
+        photo = cls.objects.filter(user__username__icontains=search_term)
+        return photo
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -19,3 +28,13 @@ class Profile(models.Model):
                 output_size = (300,300)
                 img.thumbnail(output_size)
                 img.save(self.image.path)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
