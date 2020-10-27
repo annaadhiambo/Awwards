f72e8772fd1f2ec0f3328edbf57d89d5cfe99431
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-27 07:31:35 +0300

    added test

--- anna/tests.py
+++ anna/tests.py
@@ -1,7 +1,29 @@
 from django.test import TestCase
 from .models import Post, Username
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 # Create your tests here.
 class PostTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username="testuser",password="adminpass")
+        self.user.save()
+        self.post = Post(
+            image= SimpleUploadedFile(name='test_image.jpg', content=open('..templates/default.jpg', 'rb').read(), content_type='image/jpeg'),
+            title="test title",
+            description="test description",
+            image_url="http://test.image.url.com",
+            author=user,
+            date_posted=datetime.now()
+        )
+    
+    def test_user_create(self):
+        self.post.save()
+        query = Post.objects.all()
+        self.assertTrue(len(query)>1)
+
+
     
     
